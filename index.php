d0ee99070ef1f0b8773c8f800e4fc3a9d60860c9
Raphael AVOCEGAMOU <avocegamouraph@gmail.com>
2021-05-24 19:37:37 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styles.css" />
+    <title>Formation Laravel | Solidarity</title>
+  </head>
+  <body>
+    <div class="leaderboard">
+      <header>
+        <h1 class="leaderboard__title">
+          <span class="leaderboard__title--top">Chapitre</span>
+          <span class="leaderboard__title--bottom">Par Journée</span>
+        </h1>
+      </header>
+
+      <main class="leaderboard__profiles">
+        <article class="leaderboard__profile">
+          <img
+            src="https://randomuser.me/api/portraits/men/32.jpg"
+            alt="Mark Zuckerberg"
+            class="leaderboard__picture"
+          />
+          <span class="leaderboard__name">Mark Zuckerberg</span>
+          <span class="leaderboard__value">35.7<span>B</span></span>
+        </article>
+      </main>
+    </div>
+  </body>
+</html>
